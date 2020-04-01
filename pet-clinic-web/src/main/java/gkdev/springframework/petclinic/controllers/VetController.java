1637620f5686ed0ea2bc355b4e404df774152647
Konstantin Romanov <konstantin.romanov@live.be>
2020-04-01 17:28:09 +0200

    Refactoring vets page and controller.

--- pet-clinic-web/src/main/java/gkdev/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/gkdev/springframework/petclinic/controllers/VetController.java
@@ -8,10 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @created 01/04/2020 - 14:19
  * @project pet-clinic
  */
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"", "/", "/index", "/index.html"})
     public String listVets() {
 
         return "vets/index";
