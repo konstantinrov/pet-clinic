30a000325591eedd195248425a7cfcac168b05a6
Konstantin Romanov <konstantin.romanov@live.be>
2020-04-05 18:12:05 +0200

    Adding Specialities, added to Vet. Closes #27

---
+++ pet-clinic-data/src/main/java/gkdev/springframework/petclinic/model/Speciality.java
@@ -0,0 +1,21 @@
+package gkdev.springframework.petclinic.model;
+
+/**
+ * @author Konstantin Romanov
+ * @created 05/04/2020 - 18:09
+ * @project pet-clinic
+ */
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+}
--- pet-clinic-data/src/main/java/gkdev/springframework/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/gkdev/springframework/petclinic/model/Vet.java
@@ -1,9 +1,21 @@
 package gkdev.springframework.petclinic.model;
 
+import java.util.Set;
+
 /**
  * @author Konstantin Romanov
  * @created 29/03/2020 - 14:09
  * @project pet-clinic
  */
-public class Vet extends Person{
+public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
