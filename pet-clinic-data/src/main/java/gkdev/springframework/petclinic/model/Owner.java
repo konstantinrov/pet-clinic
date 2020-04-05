1242da885cff59025fe7191acfbe1515e95d6ce7
Konstantin Romanov <konstantin.romanov@live.be>
2020-04-05 18:07:37 +0200

    Adding Visits, updating Owner with Pet relations. Closes #26

--- pet-clinic-data/src/main/java/gkdev/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/gkdev/springframework/petclinic/model/Owner.java
@@ -1,9 +1,21 @@
 package gkdev.springframework.petclinic.model;
 
+import java.util.Set;
+
 /**
  * @author Konstantin Romanov
  * @created 29/03/2020 - 14:09
  * @project pet-clinic
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
---
+++ pet-clinic-data/src/main/java/gkdev/springframework/petclinic/model/Visit.java
@@ -0,0 +1,39 @@
+package gkdev.springframework.petclinic.model;
+
+import java.time.LocalDate;
+
+/**
+ * @author Konstantin Romanov
+ * @created 05/04/2020 - 18:04
+ * @project pet-clinic
+ */
+public class Visit extends BaseEntity {
+
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
