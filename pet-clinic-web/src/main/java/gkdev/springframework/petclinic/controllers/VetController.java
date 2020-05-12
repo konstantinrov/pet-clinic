c70e7bdc90285e819373609a7e4e82a87e0a9c28
Konstantin Romanov <konstantin.romanov@live.be>
2020-05-12 13:35:15 +0200

    Adding JSON endpoint for vets. Closes #70

--- pet-clinic-web/src/main/java/gkdev/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/gkdev/springframework/petclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package gkdev.springframework.petclinic.controllers;
 
+import gkdev.springframework.petclinic.model.Vet;
 import gkdev.springframework.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 /**
  * @author Konstantin Romanov
@@ -25,5 +30,10 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
